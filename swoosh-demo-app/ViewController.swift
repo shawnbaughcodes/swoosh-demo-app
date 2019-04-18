3e05031e5e3e57b3454b8f371df288ca301af0be
Shawn Baugh II <shawnbaughcodes@gmail.com>
2019-04-18 06:07:26 -0500

    Initial Commit

---
+++ swoosh-demo-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swoosh-demo-app
+//
+//  Created by Shawn Baugh on 4/18/19.
+//  Copyright © 2019 Shawn Baugh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ swoosh-demo-appTests/swoosh_demo_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  swoosh_demo_appTests.swift
+//  swoosh-demo-appTests
+//
+//  Created by Shawn Baugh on 4/18/19.
+//  Copyright © 2019 Shawn Baugh. All rights reserved.
+//
+
+import XCTest
+@testable import swoosh_demo_app
+
+class swoosh_demo_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
